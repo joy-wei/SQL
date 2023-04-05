7de5df52c908f7b3a3306738b43a2c72380c46eb
joy-wei <54200353+joy-wei@users.noreply.github.com>
2023-04-05 18:53:04 +0800

    Update 7天连续登录用户.sql

--- 7天连续登录用户.sql
+++ 7天连续登录用户.sql
@@ -1,20 +1,14 @@
-SELECT user_id
-FROM (
-  SELECT user_id, login_date,
-    ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY login_date) AS row_number
-  FROM user_login
-) AS t1
-WHERE EXISTS (
-  SELECT *
-  FROM (
-    SELECT user_id, login_date,
-      ROW_NUMBER() OVER (PARTITION BY user_id ORDER BY login_date) AS row_number
-    FROM user_login
-  ) AS t2
-  WHERE t1.user_id = t2.user_id
-    AND t2.login_date >= t1.login_date
-    AND t2.login_date <= DATE_ADD(t1.login_date, INTERVAL 6 DAY)
-    AND t2.row_number = t1.row_number + 6
-)
-GROUP BY user_id
-HAVING COUNT(*) >= 7;
+-- 从登录表中选择每个用户的登录日期，并使用 LEAD 函数计算第七次登录日期
+SELECT 
+    COUNT(DISTINCT user_id) AS num_users 
+FROM 
+    (SELECT 
+        user_id, 
+        login_date, 
+        LEAD(login_date, 6) OVER (PARTITION BY user_id ORDER BY login_date) AS seventh_login -- 使用 DATE_ADD 函数将第一次登录日期加上 6 天，与第七次登录日期比较，若相等则说明用户连续登录 7 天
+    FROM 
+        login_table) AS t 
+WHERE 
+    seventh_login = DATE_ADD(login_date, INTERVAL 6 DAY)
+
+
