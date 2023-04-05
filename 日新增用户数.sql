ca05a7a6066d0cbcc5f730d81a9e6724c7c56a20
joy-wei <54200353+joy-wei@users.noreply.github.com>
2023-04-05 18:47:29 +0800

    Update 日新增用户数.sql

--- 日新增用户数.sql
+++ 日新增用户数.sql
@@ -1,11 +1,6 @@
-SELECT date, COUNT(DISTINCT user_id) AS new_users
-FROM user_activity AS a
-WHERE date BETWEEN '2023-03-01' AND '2023-03-21'
-  AND NOT EXISTS (
-    SELECT DISTINCT user_id,date
-    FROM user_activity AS b
-    WHERE b.user_id = a.user_id
-      AND b.date < '2023-03-01'
-  )
-GROUP BY date
-ORDER BY date
+-- 使用 LEFT JOIN 和 WHERE 子句进行表的连接和筛选，得到历史注册表和活跃表的对应关系
+-- 以历史注册表为基础，找出不在活跃表中的用户，即为新增用户
+SELECT COUNT(DISTINCT hr.user_id) AS new_users
+FROM historical_registrations hr
+LEFT JOIN active_users au ON hr.user_id = au.user_id
+WHERE au.user_id IS NULL;
