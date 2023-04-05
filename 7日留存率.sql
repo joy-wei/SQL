215a8af1150d9637bc33d61e15a7c5872a11dbf3
joy-wei <54200353+joy-wei@users.noreply.github.com>
2023-04-05 18:37:24 +0800

    Update 7日留存率.sql

--- 7日留存率.sql
+++ 7日留存率.sql
@@ -1,16 +1,15 @@
-SELECT
-  DATEDIFF(a.date, b.date) AS days_since_signup,
-  COUNT(DISTINCT b.user_id) AS total_signups,
-  COUNT(DISTINCT a.user_id) AS retained_users,
-  COUNT(DISTINCT a.user_id) / COUNT(DISTINCT b.user_id) AS retention_rate
-FROM
-  user_signup AS b
-LEFT JOIN
-  user_signup AS a
-ON
-  b.user_id = a.user_id
-  AND DATEDIFF(a.date, b.date) BETWEEN 1 AND 7
-GROUP BY
-  1
-ORDER BY
-  1
+SELECT 
+    COUNT(DISTINCT t1.user_id) AS `7_day_retention` -- 统计留存用户数
+FROM 
+    (
+    SELECT 
+        user_id, 
+        DATE(created_at) AS `day`, 
+        LEAD(DATE(created_at), 7) OVER (PARTITION BY user_id ORDER BY DATE(created_at)) AS `7th_day` -- 计算第七天的日期
+    FROM 
+        user_login_log
+    ) t1
+WHERE 
+    t1.`7th_day` = DATE_ADD(t1.`day`, INTERVAL 7 DAY) -- 满足第七天登录的用户
+    AND t1.`day` = '2023-03-10' -- 统计 2023-03-10 这一天的留存率
+
